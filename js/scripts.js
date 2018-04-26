cfb6426483095e7e6d56ba6fac134501a2955c16
Ewa <grtxhu@gmail.com>
2018-04-26 22:09:04 +0200

    Add files

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+function drawTree(a){
+ for( var i=0; i<a; i++){
+ var star = "";
+ for(var j=0; j<i; j++){
+			star += "*";
+}
+  
+   console.log(star);
+ }
+  
+}
+
+var a = prompt("Wysokość choinki");
+console.log(drawTree(a));
